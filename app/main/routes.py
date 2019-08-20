97cf178b0d1e865ec3a552df4f1e745eb01eb6da
Ben Busby <ben.busby7@gmail.com>
2019-08-19 22:56:30 -0600

    Removed room option for entering chat, updated styling of the app

--- app/main/routes.py
+++ app/main/routes.py
@@ -14,11 +14,10 @@ def index():
     form = LoginForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
-        session['room'] = form.room.data
+        session['room'] = 'dnd-room' # form.room.data
         return redirect(url_for('.chat'))
     elif request.method == 'GET':
         form.name.data = session.get('name', '')
-        form.room.data = session.get('room', '')
     return render_template('index.html', form=form)
 
 
